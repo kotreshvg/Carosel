7d5cd7a3766cbd8da03c11af6030b4346a2b4f35
sharath <kotresh.vgk@gmail.com>
2021-10-13 18:06:55 +0530

    front end mockup html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Carosal</h1>
+      <div className='body'>
+        <div className='side_bar'>
+          <div className='categories'>
+            <p>Categories</p>
+            <ul>
+              <li>People</li>
+              <li>Beaches</li>
+              <li>.</li>
+            </ul>
+          </div>
+          <div className='files'>
+            <p>Files</p>
+            <ul>
+              <li>People</li>
+              <li>Beaches</li>
+              <li>.</li>
+              <li>.</li>
+              <li>.</li>
+              <li>.</li>
+              <li>.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='main_area'>
+          <div className='carousal_diaplay'>
+            <div className='previous'><i class="fa fa-caret-left" aria-hidden="true"/></div>
+            <img className='carousal_img'></img>
+            <div className='next'><i class="fa fa-caret-right" aria-hidden="true"/></div>
+          </div>
+          <div className='thumbnails'>
+            <div className='thumb'>
+              <img src='' className='thumb_image'></img>
+              <p role='imgName'>name</p>
+            </div>
+            <div className='thumb'>
+              <img src='' className='thumb_image'></img>
+              <p role='imgName'>name</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
